b408cff4dcd059fa9a6d9ec646e9f0ffb15573c7
Joshua Matthews <jmatth93@gmail.com>
2017-02-08 00:15:06 -0600

    added stratified splitting and cofusion matrix calculations which lead me to find out the model is not working as it should

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 import sys
 
 
+np.random.seed(7)
+tf.set_random_seed(7)
+
 init_data = pd.read_csv("./HR_comma_sep.csv")
 
 # take a look at the info
@@ -55,17 +58,33 @@ print("Min value last_evaluation: {0}".format(init_data["last_evaluation"].min()
 
 # So the values are bounded between 0 and 1
 
+# Now lets look at what the percentage of the people that left vs the people that stayed
+## 0: 0.761917;  1: 0.238083
+print(init_data["left"].value_counts() / len(init_data["left"]))
 
-# Now lets create our split method and split the data
-def split_data(data, ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
+# Now lets create a stratified splitting method to ensure the data is split with the same percentages
+# of our feature of interest as the original data set as a whole.
+def stratified_split_data(data, ratio):
+    # Grab the data into its own category
+    stayed_data = data.loc[data["left"] == 0]
+    left_data = data.loc[data["left"] == 1]
+    # mix up the data
+    stayed_data = stayed_data.iloc[np.random.permutation(len(stayed_data))]
+    left_data = left_data.iloc[np.random.permutation(len(left_data))]
+    test_stayed_set_size = int(len(stayed_data) * ratio)
+    test_left_set_size = int(len(left_data) * ratio)
+    # Concatenate the partitioned data
+    train_set = pd.concat([stayed_data[test_stayed_set_size:], left_data[test_left_set_size:]], ignore_index=True)
+    test_set = pd.concat([stayed_data[:test_stayed_set_size], left_data[:test_left_set_size]], ignore_index=True)
+    # Now mix up the concatenated data
+    train_shuffled_indices = np.random.permutation(len(train_set))
+    test_shuffled_indices = np.random.permutation(len(test_set))
+    return train_set.iloc[train_shuffled_indices], test_set.iloc[test_shuffled_indices]
 
-train_set, test_set = split_data(init_data, 0.2)
+train_set, test_set = stratified_split_data(init_data, 0.2)
 
+# Now lets make sure we still have the same percentages
+print(train_set["left"].value_counts() / len(train_set["left"]))
 
 data = (train_set.drop("left", axis=1)).values
 data_labels = train_set["left"].values
@@ -77,20 +96,22 @@ n_samples = data.shape[0]
 X_init = tf.placeholder(tf.float32, [None, num_features])
 Y_init = tf.placeholder(tf.float32, [None, 1])
 
-w_1 = tf.Variable(tf.random_normal([num_features, 10]))
-b_1 = tf.Variable(tf.random_normal([10]))
+#w_1 = tf.Variable(tf.random_normal([num_features, 10]))
+#b_1 = tf.Variable(tf.random_normal([10]))
 
-layer_1 = tf.sigmoid(tf.add(tf.matmul(X_init, w_1), b_1))
+#layer_1 = tf.sigmoid(tf.add(tf.matmul(X_init, w_1), b_1))
 
-w_2 = tf.Variable(tf.random_normal([10, 1]))
-b_2 = tf.Variable(tf.random_normal([1]))
+w_2 = tf.Variable(tf.truncated_normal([num_features, 1], stddev=0.01))
+b_2 = tf.Variable(tf.truncated_normal([1], stddev=0.01))
 
-output_layer = tf.sigmoid(tf.add(tf.matmul(layer_1, w_2), b_2))
+output_layer = tf.nn.softmax(tf.add(tf.matmul(X_init, w_2), b_2))
 
-cost = -tf.reduce_mean(tf.multiply(Y_init, tf.log(output_layer)))
+#cost = -tf.reduce_mean(tf.multiply(Y_init, tf.log(output_layer)) )
+
+cost = tf.reduce_mean(tf.pow(tf.subtract(output_layer, Y_init), 2))
 
 ## mess around with learning rate 
-optimizer = tf.train.AdamOptimizer(1e-3).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(1e-1).minimize(cost)
 
 init = tf.global_variables_initializer()
 
@@ -101,16 +122,54 @@ sess.run(init)
 loss_values = []
 
 
-num_epochs = 1000
+num_epochs = 500
+batch_size = 100
+count = len(data)
 for epoch in range(num_epochs):
+    start_n = 0
+    c = None
     _, c = sess.run([optimizer, cost], feed_dict={X_init:data, Y_init:data_labels})
-    loss_values.append(c)
+#    while start_n < count:
+#        _, c = sess.run([optimizer, cost], feed_dict={X_init:data[start_n:(start_n + batch_size)], Y_init:data_labels[start_n:(start_n + batch_size)]})
+#        sys.stdout.write("Batch:{0}/{1} cost:{2}\r".format(start_n+batch_size, count, c))
+#        sys.stdout.flush()
+#        start_n += batch_size
+#        loss_values.append(c)
+#    print("")
     sys.stdout.write("Epoch: {0}/{1} cost: {2}\r".format(epoch+1, num_epochs, c))
     sys.stdout.flush()
+#    print("")
 
 
 ## right now getting final cost around 0.00132
-print("Final cost = {0}".format(sess.run(cost, feed_dict={X_init:data, Y_init:data_labels}) ) )
+print("Final cost = {0} weights: {1} biases: {2}".format(sess.run(cost, feed_dict={X_init:data, Y_init:data_labels}), sess.run(w_2), sess.run(b_2) ) )
+
+
+# Lets take a look at the confusion matrix
+predictions = sess.run(output_layer, feed_dict={X_init:data})
+
+print(predictions[:5])
+print(data_labels[:5])
+
+
+def confusion_matrix(pred_data, act_data, threshold=0.7):
+    stayed_true = 0
+    stayed_false = 0
+    left_true = 0
+    left_false = 0
+    for i in range(len(pred_data)):
+        if pred_data[i][0] >= threshold and act_data[i][0] == 1:
+            left_true += 1
+        elif pred_data[i][0] < threshold and act_data[i][0] == 1:
+            left_false += 1
+        elif pred_data[i][0] >= threshold and act_data[i][0] == 0:
+            stayed_false += 1
+        elif pred_data[i][0] < threshold and act_data[i][0] == 0:
+            stayed_true += 1
+    print("Stayed True: {0}\nStayed False: {1}\nLeft True: {2}\nLeft False: {3}".format(stayed_true, stayed_false, left_true, left_false))
+
+confusion_matrix(predictions, data_labels)
+
 
 
 ## test is getting comparable numbers as well
